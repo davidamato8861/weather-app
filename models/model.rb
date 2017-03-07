2e2cb907a779d025febe9488b4a9eb8f9f82043b
davidamato8861 <davida8861@hstat.org>
2017-03-07 19:23:09 +0000

    model setup

---
+++ models/model.rb
@@ -0,0 +1,18 @@
+require 'pp'
+require 'yahoo-weather'
+
+Yahoo.authenticate(ENV["YAHOO_ID"], ENV["YAHOO_SECRET"])
+class Location
+  attr_reader :temp
+  
+  def initialize(temp,location)
+      @temp = temp
+      @location = location 
+  end
+  
+  
+  
+  
+  
+  
+end
