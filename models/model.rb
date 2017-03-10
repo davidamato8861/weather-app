e3af072df5143d90c3d7a0d66ece98c62d17cf07
luism6058 <luism6058@hstat.org>
2017-03-10 18:50:33 +0000

    added results.erb

--- models/model.rb
+++ models/model.rb
@@ -4,7 +4,7 @@ require 'weather-api'
 
 # look up WOEID via http://weather.yahoo.com; enter location by city
 # name or zip and WOEID is at end of resulting page url.
-response = Weather.lookup_by_location('Des Moines', Weather::Units::FAHRENHEIT)
+response = Weather.lookup_by_location('California', Weather::Units::FAHRENHEIT)
 print <<EOT
 #{response.title}
 #{response.condition.temp} degrees
