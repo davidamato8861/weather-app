7a710947de57b6928fd38940889d88ec1832ea26
davidamato8861 <davida8861@hstat.org>
2017-03-13 17:48:27 +0000

    rename result

--- models/model.rb
+++ models/model.rb
@@ -2,6 +2,13 @@ require 'rubygems'
 require 'weather-api'
 
 
+class User_Weather
+  def initialize()
+end                 
+
+
+
+end
 # look up WOEID via http://weather.yahoo.com; enter location by city
 # name or zip and WOEID is at end of resulting page url.
 response = Weather.lookup_by_location('California', Weather::Units::FAHRENHEIT)
@@ -10,5 +17,6 @@ print <<EOT
 #{response.condition.temp} degrees
 #{response.condition.text}
 EOT
+  
   
   
