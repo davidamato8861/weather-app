baf472f3880dc484b1ddf04b1024504063c86b15
davidamato8861 <davida8861@hstat.org>
2017-03-15 20:57:45 +0000

    remove class user

--- models/model.rb
+++ models/model.rb
@@ -2,22 +2,16 @@ require 'rubygems'
 require 'weather-api'
 
 
-class User_Weather
-  def initialize()
-    
-end                 
 
-
-
-end
 # look up WOEID via http://weather.yahoo.com; enter location by city
 # name or zip and WOEID is at end of resulting page url.
-response = Weather.lookup_by_location('California', Weather::Units::FAHRENHEIT)
+response = Weather.lookup_by_location('Texas', Weather::Units::FAHRENHEIT)
 print <<EOT
 #{response.title}
 #{response.condition.temp} degrees
 #{response.condition.text}
 EOT
   
+
   
   
